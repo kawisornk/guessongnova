f1661d0c7ba8a81923c1c382b379e91b4fa04d8a
Kawisorn Kamtue <kawisorn.kamtue@gmail.com>
2019-02-15 11:53:00 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 
+# -*- coding: utf-8 -*-
+#import sys
+#from importlib import reload
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+from flask import request
+from flask import Flask, url_for
 import numpy as np
 import pandas as pd
 from collections import Counter
 import deepcut
-# ## Encoding function
+import speech_recognition as sr
+
+## Encoding function
 
 # In[10]:
-
 import pickle
 def save_obj(obj, name ):
 	with open('obj/'+ name + '.pkl', 'wb') as f:
@@ -16,19 +24,18 @@ def load_obj(name):
 	with open('obj/' + name + '.pkl', 'rb') as f:
 		return pickle.load(f)
 
+app = Flask(__name__)
 
-
-import speech_recognition as sr
-
-if __name__ == '__main__':
+@app.route('/api/listen')
+def listen():
 	cnt = Counter()
-	karaoke_dict = load_obj("karaoke_dict")
+	karaoke_dict = load_obj("karaoke_dict_new")
 
 	# Record Audio
 	r = sr.Recognizer()
 	with sr.Microphone() as source:
-	    print("ร้องเพลงสิ!")
-	    audio = r.listen(source)
+	    # print("ร้องเพลงสิิ!")
+	    audio = r.listen(source, phrase_time_limit=15)
 	 
 	# Speech recognition using Google Speech Recognition
 	try:
@@ -36,15 +43,20 @@ if __name__ == '__main__':
 	    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
 	    # instead of `r.recognize_google(audio)`
 	    text = r.recognize_google(audio, language='th-TH')
-	    print("You said: " + text)
-	    print("Guessing the song....")
+	    # print("You said: " + text)
+	    # print("Guessing the song....")
 	    tokens = deepcut.tokenize(text)
 	    for j in range(len(tokens)-6):
 	    	words = "".join(tokens[j:j+6])
 	    	if words in karaoke_dict:
 	    		cnt[karaoke_dict[words]] += 1
-	    print(cnt)
+	    return text+str(cnt)
 	except sr.UnknownValueError:
-	    print("Google Speech Recognition could not understand audio")
+	    return "คุณเป็นนักร้องเสียงเพี้ยนนนนนนนน"
 	except sr.RequestError as e:
-	    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+	    return "พังจ้าาา; {0}".format(e)
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5555, debug=True)
+
